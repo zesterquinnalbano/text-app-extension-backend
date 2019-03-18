b71acaead6b93b9b3bd6404529d89a5e5e133d6d
Zester Quinn Albano <zesterquinn.albano@gmail.com>
2019-03-18 14:59:05 +0800

    add groups when adding csv

--- app/Imports/ContactImport.php
+++ app/Imports/ContactImport.php
@@ -7,6 +7,7 @@ use Auth;
 use Carbon\Carbon;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToCollection;
+use App\ContactGroup;
 
 class ContactImport implements ToCollection
 {
@@ -21,11 +22,16 @@ class ContactImport implements ToCollection
                 var_dump($row);
                 $pos = strpos($row['2'], '+');
 
+                $contactGroup = ContactGroup::firstOrCreate([
+                    'name' => $row[3]
+                ]);
+
                 if ($pos === false) {
                     $contacts[] = [
                         'firstname' => $row[0],
                         'lastname' => $row[1],
                         'contact_number' => '+' . $row[2],
+                        'contact_group_id' => $contactGroup->id,
                         'created_at' => Carbon::now(),
                         'updated_at' => Carbon::now(),
                         'created_by' => Auth::id(),
@@ -35,6 +41,7 @@ class ContactImport implements ToCollection
                         'firstname' => $row[0],
                         'lastname' => $row[1],
                         'contact_number' => $row[2],
+                        'contact_group_id' => $contactGroup->id,
                         'created_at' => Carbon::now(),
                         'updated_at' => Carbon::now(),
                         'created_by' => Auth::id(),
